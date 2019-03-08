57ac4b64a2496b649e219eef3597e1c1562b4f5d
Tácio Nery <taiconery@gmail.com>
2019-03-08 10:12:26 -0300

    Machine Head was born

---
+++ MachineHead.java
@@ -0,0 +1,51 @@
+package com.thnery;
+import robocode.*;
+import java.awt.Color;
+
+/**
+ * MachineHead - a robot by thnery
+ */
+public class MachineHead extends Robot {
+	boolean peek;
+	double distance;
+
+	public void run() {
+		// Set Machine Head War Paint
+		setColors(Color.blue, Color.yellow, Color.green);
+
+		// Set distance to move
+		distance = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		peek = false;
+
+		// Go to the first wall
+		turnLeft(getHeading() % 90);
+		ahead(distance);
+
+		// Prepare guns
+		peek = true;
+		turnGunRight(0);
+		turnLeft(90);
+
+		// Time to kill
+		while(true) {
+			peek = true;
+			ahead(distance);
+			peek = false;
+
+			turnLeft(90);
+		}
+	}
+
+	public void onHitRobot(HitRobotEvent event) {
+		if (event.getBearing() > -90 && event.getBearing() < 90) {
+			back(100);
+		} else {
+			ahead(100);
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent event) {
+		fire(6);
+		if (peek) { scan(); }
+	}
+}
