a8d10cb2b4425b79f1bf071e8fa827c3df2c0ed9
Tácio Nery <taiconery@gmail.com>
2019-03-09 14:20:32 -0300

    fix when robot hit wall and stop moving

--- MachineHead.java
+++ MachineHead.java
@@ -6,46 +6,52 @@ import java.awt.Color;
  * MachineHead - a robot by thnery
  */
 public class MachineHead extends Robot {
-	boolean peek;
-	double distance;
+    boolean peek;
+    double distance;
 
-	public void run() {
-		// Set Machine Head War Paint
-		setColors(Color.white, Color.red, Color.black);
+    public void run() {
+        // Set Machine Head War Paint
+		setBodyColor(Color.black);
+		setGunColor(Color.white);
+		setRadarColor(Color.white);
+		setBulletColor(Color.pink);
+		setScanColor(Color.cyan);
 
-		// Set distance to move
-		distance = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
-		peek = false;
+        // Set distance to move
+        distance = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+        peek = false;
 
-		// Go to the first wall
-		turnLeft(getHeading() % 90);
-		ahead(distance);
+        // Go to the first wall
+        turnLeft(getHeading() % 90);
+        ahead(distance);
 
-		// Prepare guns
-		peek = true;
-		turnGunRight(0);
-		turnLeft(90);
+        // Prepare guns
+        peek = true;
+        turnGunRight(-90);
+        turnLeft(90);
 
-		// Time to kill
-		while(true) {
-			peek = true;
-			ahead(distance);
-			peek = false;
+        // Time to kill
+        while (true) {
+            peek = true;
+            ahead(distance);
+            peek = false;
 
-			turnLeft(90);
-		}
-	}
+            turnLeft(90);
+        }
+    }
 
-	public void onHitRobot(HitRobotEvent event) {
-		if (event.getBearing() > -90 && event.getBearing() < 90) {
-			back(100);
-		} else {
-			ahead(100);
-		}
-	}
+    public void onHitRobot(HitRobotEvent event) {
+        if (event.getBearing() > -90 && event.getBearing() < 90) {
+            back(100);
+        } else {
+            ahead(100);
+        }
+    }
 
-	public void onScannedRobot(ScannedRobotEvent event) {
-		fire(6);
-		if (peek) { scan(); }
-	}
+    public void onScannedRobot(ScannedRobotEvent event) {
+        fire(6);
+        if (peek) {
+            scan();
+        }
+    }
 }
